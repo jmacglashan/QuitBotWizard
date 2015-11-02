ecab302bfdc8d651eaade96368f4f63d0f1f45dc
jmacglashan <jmacglashan@gmail.com>
2015-11-02 09:41:25 -0500

    small changes?

--- src/tests/drinkinprompts/DLimits.java
+++ src/tests/drinkinprompts/DLimits.java
@@ -25,13 +25,13 @@ public class DLimits extends RobotPrompt{
 
 	@Override
 	public String generatePrompt() {
-		String base = "The federal guidelines drinking that is unlikely to lead to health and social problems in " +
-				"healthy adults are that: ";
+		String base = "Federal guidelines for drinking limits that are unlikely to lead to health and social " +
+				"problems in healthy adults are that: ";
 		if(this.sex.equals("male")){
-			base += "Men drink no more than 4 drinks on any day and not more than 14 drinks in any week.";
+			base += "Men drink no more than 4 drinks on any one day and less than 15 drinks in any week.";
 		}
 		else{
-			base += "Women drink no more than 3 drinks on any day and not more than 7 drinks in any week.";
+			base += "Women drink no more than 3 drinks on any one day and less than 8 drinks in any week.";
 		}
 
 		base += "\nA drink is defined as one beer, one shot of 80-proof liquor, or a 5 oz. glass of wine. \n" +
